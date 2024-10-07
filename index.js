d4760ceb33099a548de0143cf5752e7468a1e40a
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2024-10-06 21:40:54 -0700

    develop:connection wth web socket in both back+fast end

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-const express = require('express')
+const express = require('express');
+/**----app setup---- */
 const app = express();
-
+const socket = require('socket.io');
 require('dotenv').config()
 
-app.listen(process.env.PORT, () => {
+/**----server setup---- */
+let server = app.listen(process.env.PORT, () => {
     console.log('Server is listening to '+ process.env.PORT);
-})
+});
 
+/**----route setup---- */
 app.get('/',(req, res) => {
     res.sendFile(__dirname+'/public/index.html')
-})
+});
+
+/**----socket setup---- */
+let io = socket(server);
+io.on("connection", (socket) => {
+    console.log("socket connection connected" + socket.id);
+});
